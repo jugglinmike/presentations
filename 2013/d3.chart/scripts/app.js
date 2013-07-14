383f30272fdb91a5be15157994abe711f8b24f90
Mike Pennisi <mike@mikepennisi.com>
2013-07-14 12:02:23 -0400

    Add more content

--- 2013/d3.chart/scripts/app.js
+++ 2013/d3.chart/scripts/app.js
@@ -1,11 +1,35 @@
 (function() {
 
-var s = Array.prototype.slice.call(document.querySelectorAll("#impress > .step"));
+var toArray = Array.prototype.slice.call.bind(Array.prototype.slice);
+var textProp = "innerText" in document.createElement("div") ?
+	"innerText" : "textContent";
+
+var s = toArray(document.querySelectorAll("#impress > .step"));
 var idx = 0;
 s.forEach(function(elem) {
+	var yPos = idx++ * 1500;
 	if (!elem.getAttribute("data-y")) {
-		elem.setAttribute("data-y",idx++*1500);
+		elem.setAttribute("data-y", yPos);
 	}
+	toArray(elem.querySelectorAll(".step")).forEach(function(elem) {
+		elem.setAttribute("data-y", yPos);
+		elem.setAttribute("data-z", 100);
+	});
+});
+
+// Normalize indentation of code samples
+toArray(document.querySelectorAll("pre.code > code")).forEach(function(codeElem) {
+	var code = codeElem[textProp];
+	var indent;
+
+	// Strip leading newline characters and trailing whitespace
+	code = code.replace(/^[\r\n]*|\s*$/g, "");
+	// Detect the "baseline" indentation as defined by first line of code
+	indent = (code.match(/^\s+/) || [""])[0];
+	// Remove "baseline" indentation from each line
+	code = code.replace(new RegExp("^" + indent, "gm"), "");
+
+	codeElem[textProp] = code;
 });
 
 impress().init();
