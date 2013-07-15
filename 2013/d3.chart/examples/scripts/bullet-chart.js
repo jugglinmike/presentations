867c53e591b73376c3a03fe5d44a29872d073790
Mike Pennisi <mike@mikepennisi.com>
2013-07-14 21:48:21 -0400

    Remove unused code

--- 2013/d3.chart/examples/scripts/bullet-chart.js
+++ 2013/d3.chart/examples/scripts/bullet-chart.js
@@ -1,40 +1,24 @@
-// Chart design based on the recommendations of Stephen Few. Implementation
-// based on the work of Clint Ivy, Jamie Love, and Jason Davies.
-// http://projects.instantcognition.com/protovis/bulletchart/
+// Chart design based on original implementation by Mike Bostock:
+// http://bl.ocks.org/mbostock/4061961
 d3.chart("Bullet", {
 	initialize: function() {
 		this.xScale = d3.scale.linear();
 
 		// Default configuration
 		this.duration(0);
-		this.markers(bulletMarkers);
-		this.measures(bulletMeasures);
+		this.markers(function(d) {
+			return d.markers;
+		});
+		this.measures(function(d) {
+			return d.measures;
+		});
 		this.width(380);
 		this.height(30);
 		this.tickFormat(this.xScale.tickFormat(8));
 		this.orient("left"); // TODO top & bottom
-		this.ranges(bulletRanges);
-
-  // For each small multiple…
-  //function bullet(g) {
-      //var rangez = this._ranges.call(this.base, d, i).slice().sort(d3.descending),
-      //    markerz = this._markers.call(this.base, d, i).slice().sort(d3.descending),
-      //    measurez = this._measures.call(this.base, d, i).slice().sort(d3.descending),
-      //    g = this.base;
-
-      // Compute the new x-scale.
-      //var x1 = d3.scale.linear()
-      //    .domain([0, Math.max(rangez[0], markerz[0], measurez[0])])
-      //    .range(this._reverse ? [this.width(), 0] : [0, this.width()]);
-
-      // Retrieve the old x-scale, if this is an update.
-      //var x0 = d3.scale.linear()
-      //    .domain([0, Infinity])
-      //    .range(x1.range());
-
-      // Derive width-scales from the x-scales.
-      //var w0 = bulletWidth(x0),
-      //    w1 = bulletWidth(x1);
+		this.ranges(function(d) {
+			return d.ranges;
+		});
 
 		this.layer("ranges", this.base.append("g").classed("ranges", true), {
 			dataBind: function(data) {
@@ -65,30 +49,10 @@ d3.chart("Bullet", {
 				}
 			}
 		});
-		/*
-      // Update the range rects.
-      var range = g.selectAll("rect.range")
-          .data(rangez);
-
-      range.enter().append("rect")
-          .attr("class", function(d, i) { return "range s" + i; })
-          .attr("width", w0)
-          .attr("height", height)
-          .attr("x", this._reverse ? x0 : 0)
-        .transition()
-          .duration(duration)
-          .attr("width", w1)
-          .attr("x", this._reverse ? x1 : 0);
-
-      range.transition()
-          .duration(duration)
-          .attr("x", this._reverse ? x1 : 0)
-          .attr("width", w1)
-          .attr("height", height);
-		*/
 
 		this.layer("measures", this.base.append("g").classed("measures", true), {
 			dataBind: function(data) {
+				// This layer operates on "measures" data
 				data = data.measures;
 
 				return this.selectAll("rect.measure").data(data);
@@ -114,33 +78,12 @@ d3.chart("Bullet", {
 				}
 			}
 		});
-		/*
-      // Update the measure rects.
-      var measure = g.selectAll("rect.measure")
-          .data(measurez);
-
-      measure.enter().append("rect")
-          .attr("class", function(d, i) { return "measure s" + i; })
-          .attr("width", w0)
-          .attr("height", height / 3)
-          .attr("x", this._reverse ? x0 : 0)
-          .attr("y", height / 3)
-        .transition()
-          .duration(duration)
-          .attr("width", w1)
-          .attr("x", this._reverse ? x1 : 0);
-
-      measure.transition()
-          .duration(duration)
-          .attr("width", w1)
-          .attr("height", height / 3)
-          .attr("x", this._reverse ? x1 : 0)
-          .attr("y", height / 3);
-		*/
 
 		this.layer("markers", this.base.append("g").classed("markers", true), {
 			dataBind: function(data) {
+				// This layer operates on "markers" data
 				data = data.markers;
+
 				return this.selectAll("line.marker").data(data);
 			},
 			insert: function() {
@@ -167,32 +110,6 @@ d3.chart("Bullet", {
 				}
 			}
 		});
-		/*
-      // Update the marker lines.
-      var marker = g.selectAll("line.marker")
-          .data(markerz);
-
-      marker.enter().append("line")
-          .attr("class", "marker")
-          .attr("x1", x0)
-          .attr("x2", x0)
-          .attr("y1", height / 6)
-          .attr("y2", height * 5 / 6)
-        .transition()
-          .duration(duration)
-          .attr("x1", x1)
-          .attr("x2", x1);
-
-      marker.transition()
-          .duration(duration)
-          .attr("x1", x1)
-          .attr("x2", x1)
-          .attr("y1", height / 6)
-          .attr("y2", height * 5 / 6);
-		*/
-
-      // Compute the tick format.
-      //var format = this.tickFormat() || 
 
 		this.layer("ticks", this.base.append("g").classed("ticks", true), {
 			dataBind: function() {
@@ -221,7 +138,10 @@ d3.chart("Bullet", {
 			},
 			events: {
 				enter: function() {
-					this.attr("transform", bulletTranslate(this.chart().xScale))
+					var chart = this.chart();
+					this.attr("transform", function(d) {
+							return "translate(" + chart.xScale(d) + ",0)";
+						})
 						.style("opacity", 1e-6);
 				},
 				"merge:transition": function() {
@@ -229,7 +149,9 @@ d3.chart("Bullet", {
 					var height = chart.height();
 
 					this.duration(chart.duration())
-						.attr("transform", bulletTranslate(chart.xScale))
+						.attr("transform", function(d) {
+							return "translate(" + chart.xScale(d) + ",0)";
+						})
 						.style("opacity", 1);
 					this.select("line")
 						.attr("y1", height)
@@ -240,65 +162,16 @@ d3.chart("Bullet", {
 				"exit:transition": function() {
 					var chart = this.chart()
 					this.duration(chart.duration())
-						.attr("transform", bulletTranslate(chart.xScale))
+						.attr("transform", function(d) {
+							return "translate(" + chart.xScale(d) + ",0)";
+						})
 						.style("opacity", 1e-6)
 						.remove();
 				}
 			}
 		});
 
-		/*
-      // Update the tick groups.
-      var tick = g.selectAll("g.tick")
-          .data(x1.ticks(8), function(d) {
-            return this.textContent || format(d);
-          });
-
-      // Initialize the ticks with the old scale, x0.
-      var tickEnter = tick.enter().append("g")
-          .attr("class", "tick")
-          .attr("transform", bulletTranslate(x0))
-          .style("opacity", 1e-6);
-
-      tickEnter.append("line")
-          .attr("y1", height)
-          .attr("y2", height * 7 / 6);
-
-      tickEnter.append("text")
-          .attr("text-anchor", "middle")
-          .attr("dy", "1em")
-          .attr("y", height * 7 / 6)
-          .text(format);
-
-      // Transition the entering ticks to the new scale, x1.
-      tickEnter.transition()
-          .duration(duration)
-          .attr("transform", bulletTranslate(x1))
-          .style("opacity", 1);
-
-      // Transition the updating ticks to the new scale, x1.
-      var tickUpdate = tick.transition()
-          .duration(duration)
-          .attr("transform", bulletTranslate(x1))
-          .style("opacity", 1);
-
-      tickUpdate.select("line")
-          .attr("y1", height)
-          .attr("y2", height * 7 / 6);
-
-      tickUpdate.select("text")
-          .attr("y", height * 7 / 6);
-
-      // Transition the exiting ticks to the new scale, x1.
-      tick.exit().transition()
-          .duration(duration)
-          .attr("transform", bulletTranslate(x1))
-          .style("opacity", 1e-6)
-          .remove();
-		*/
-    d3.timer.flush();
-  //}
-  //return bullet;
+		d3.timer.flush();
 	},
 
 	transform: function(data) {
@@ -367,30 +240,4 @@ d3.chart("Bullet", {
 		this._duration = x;
 		return this;
 	}
-
 });
-
-function bulletRanges(d) {
-  return d.ranges;
-}
-
-function bulletMarkers(d) {
-  return d.markers;
-}
-
-function bulletMeasures(d) {
-  return d.measures;
-}
-
-function bulletTranslate(x) {
-  return function(d) {
-    return "translate(" + x(d) + ",0)";
-  };
-}
-
-function bulletWidth(x) {
-  var x0 = x(0);
-  return function(d) {
-    return Math.abs(x(d) - x0);
-  };
-}
